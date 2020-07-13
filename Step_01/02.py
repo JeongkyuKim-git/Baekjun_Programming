3c6eb608d3bac34272269addbd772287defecea7
JeongkyuKim-git <66001539+JeongkyuKim-git@users.noreply.github.com>
2020-07-13 14:26:43 +0900

    Create 02.py

---
+++ Step_01/02.py
@@ -0,0 +1,29 @@
+# Baekjun_Programming
+
+# Title : We love kriii
+# C:\Users\jkkim\PycharmProjects\Beckjun
+# -*- coding: utf-8 -*-
+#
+# -----------------------------------------------------------------------------------------------------------
+#  Programming
+#
+#  Author       : Jeongkyu Kim
+#  E-mail       : jkkim@mme.dongguk.edu
+#  Version      : 0.1
+#  Rev. Date    : July. 13, 2020
+#
+#  First, I used to program name is PyCham
+#   - Python 3.0
+#   - Site url : Refer to https://www.acmicpc.net/step/1
+#
+#  문제 : 강한친구 대한육군 x2
+#  입력 : 본 문제는 입력이 없다.
+#  출력 : 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+# -----------------------------------------------------------------------------------------------------------
+"""
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+"""
+
+print("""강한친구 대한육군 
+강한친구 대한육군""")
