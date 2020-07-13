ff4aef785c8ee87c85c4d4149257881f6643d33c
JeongkyuKim-git <66001539+JeongkyuKim-git@users.noreply.github.com>
2020-07-13 14:29:57 +0900

    Update 01.py

--- Step_01/01.py
+++ Step_01/01.py
@@ -5,7 +5,7 @@
 # -*- coding: utf-8 -*-
 #
 # -----------------------------------------------------------------------------------------------------------
-#  Programming
+#  01-01-Programming
 #
 #  Author       : Jeongkyu Kim
 #  E-mail       : jkkim@mme.dongguk.edu
@@ -15,6 +15,12 @@
 #  First, I used to program name is PyCham
 #   - Python 3.0
 #   - Site url : Refer to https://www.acmicpc.net/step/1
+#
+#  
+#  요구사항 : 시간 제한 (1초), 메모리 제한 (128 MB), 정답 비율 (43.559%)
+#  문제 : Hello World!를 출력하시오.
+#  입력 : 본 문제는 입력이 없다.
+#  출력 : Hello World!를 출력하시오.
 # -----------------------------------------------------------------------------------------------------------
 
 print("Hello World!")
