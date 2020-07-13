1178abbbb8fbe18dfe506b4d90b0d71df5315892
JeongkyuKim-git <66001539+JeongkyuKim-git@users.noreply.github.com>
2020-07-13 14:28:01 +0900

    Update 02.py

--- Step_01/02.py
+++ Step_01/02.py
@@ -15,7 +15,8 @@
 #  First, I used to program name is PyCham
 #   - Python 3.0
 #   - Site url : Refer to https://www.acmicpc.net/step/1
-#
+#  
+#  요구사항 : 시간 제한 (1초), 메모리 제한 (256 MB), 정답 비율 (69.882%)
 #  문제 : 강한친구 대한육군 x2
 #  입력 : 본 문제는 입력이 없다.
 #  출력 : 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
